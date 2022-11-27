e11b572bc74f463bff0f6595c86df455817c4946
yunisaliosman <yunis.osman@student.moringaschool.com>
2022-11-27 18:20:13 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+ function destructivelyAppendCat(name){
+     name = "Ralph"
+     cats.push("Ralph");
+ }
+ function destructivelyPrependCat(name){
+     name = "Bob"
+     cats.unshift("Bob");
+ }
+ function destructivelyRemoveLastCat(){
+     cats.pop();
+ }
+ function destructivelyRemoveFirstCat(){
+     cats.shift();
+ }
+ function appendCat(name){
+     name = "Broom"
+     const moreCats = [...cats, "Broom"];
+     return moreCats;
+ }
+ function prependCat(name){
+     name = "Arnold"
+     const moreCats2 = ["Arnold", ...cats];
+     return moreCats2;
+ }
+ function removeLastCat(){
+     const lessCats = cats.slice(0, 2);
+     return lessCats;
+ }
+ function removeFirstCat(){
+     const lessCats2 = cats.slice(1)
+     return lessCats2;
+ }
